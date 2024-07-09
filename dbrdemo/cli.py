d4d92861f1a20eadd244c95fb6ac81e0d875dcb4
Grzegorz Rusin <grzegorz.rusin@databricks.com>
2024-07-09 14:26:22 +0200

    fancy cli

--- dbrdemo/cli.py
+++ dbrdemo/cli.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+from databricks.sdk import WorkspaceClient
+from . import spark
 
 logger = logging.getLogger(__name__)
 
@@ -7,13 +9,15 @@ logger = logging.getLogger(__name__)
 def cli_foobar():
     parser = argparse.ArgumentParser(add_help=True, description="Sample CLI")
 
-    parser.add_argument('--foo', default=1, type=int, required=False, help="FOO Counter")
-    parser.add_argument('--bar', default=1, type=int, required=False, help="BAR Counter")
+    parser.add_argument('--foo', default="foo", type=str, required=False, help="FOO text")
+    parser.add_argument('--bar', default="bar", type=str, required=False, help="BAR text")
 
     args = parser.parse_args()
 
-    for _ in range(args.foo):
-        logger.info("FOO!!!")
+    w = WorkspaceClient()
+    logger.info(f"Current cluster id: {w.config.cluster_id!r}")
+    cluster = w.clusters.get(w.config.cluster_id)
+    logger.info(f"Current cluster name: {cluster.cluster_name!r}")
 
-    for _ in range(args.bar):
-        logger.warning("BAR!!!")
+    df = spark.createDataFrame([[args.foo, args.bar]], "foo string, bar string")
+    df.show(truncate=False)
